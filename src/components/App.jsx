e59ed280ad213298539ddeb270266d481e7eadd6
Dmitriy Gonharov <my.webpage057@gmail.com>
2022-09-20 02:11:15 +0300

    add 2d task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,13 @@
 import { Component } from 'react'
-import { nanoid } from 'nanoid'
-export class App extends Component {
-  state = {
-    contacts: [],
-    name: ''
-  }
-  nameId = nanoid()
-  contactsId = nanoid()
-  addContact = (contact) => {
-    this.setState((prev)=>{
-      return {
-        contacts: [...prev.contacts,contact]
-      }
-    })
-  }
-  handleChange = (e) => {
-    const {name, value} = e.target
-    this.setState ({[name]: value }) 
-  }
-  handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(e);
-    // this.props.onSubmit({name})
-    const {name} = this.state;
-    this.setState({
-      name: ''
-      })
-      console.log(name);
-  }
+import {Contacts} from 'components/Contacts/Contacts'
+
+export  class App extends Component {
   render() {
-    const {nameId, handleSubmit} = this;
-    // const {addContact} = this;
     return (
-    <div>     
-      <h1>Phonebook</h1> 
-      <form onSubmit={handleSubmit}>
-        <div >
-          <label htmlFor={nameId}>Name</label>
-          <input
-          onChange={this.handleChange}
-          value={this.state.name}
-          id ={nameId}
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          />
-          <button>Add contact</button>
-        </div>
-      </form>
-    </div>
+      <div>
+        <h1>Phonebook</h1> 
+        <Contacts />
+      </div>
     )
   }
 }
-
